dfdf8491a193ef80e73c1ab09dd7141b5d37d0fd
GabrielPavanati <62730052+GabrielPavanati@users.noreply.github.com>
2023-07-19 20:22:54 -0300

    modified:   assets/Avatar.png 	modified:   assets/avatar-light.png 	modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function toggleMode() {
 
     if(html.classList.contains('light')) {
         html.classList.remove('light')
-        img.setAttribute('src', './assets/Avatar.png')
-        img.setAttribute('alt', 'Foto de perfil de Gabriel Pavanati.')
+        img.setAttribute('src', './assets/avatar.png')
+        img.setAttribute('alt', 'Foto de perfil de Gabriel Pavanati em modo escuro')
     } else {
         html.classList.add('light')
         img.setAttribute('src', './assets/avatar-light.png')
-        img.setAttribute('alt', 'Foto de perfil de Gabriel Pavanati na praia.')
+        img.setAttribute('alt', 'Foto de perfil de Gabriel Pavanati em modo claro.')
     }
 }
